149dba665327f236ca017161ac5d16708dacbfba
mdunaj <mdunaj@umich.edu>
2016-10-18 14:28:43 -0400

    Add ToDos regarding app id
    We’re not sure if this is required as we can only find our skill id,
    not an app id. However all examples posted by Amazon use an app id

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var Alexa = require('alexa-sdk');
 
 var SKILL_NAME = 'X K C D';
 
+// ToDo: Find our app id (skill id?) and included it if needed
+// var APP_ID = "";
+
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
+    // TODO: uncomment if needed
+    // alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
